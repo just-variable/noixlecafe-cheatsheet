8d5d9ba04030f2e25dab2d24fee933ee26e162e6
Simon <simonhaidar2@icloud.com>
2025-10-20 19:52:03 +0300

    asdf

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "../contexts/AuthContext";
 import ProtectedRoute from "./routes/ProtectedRoute";
 import Login from "./Login/Login";
@@ -8,18 +8,16 @@ import RecipePage from "./RecipePage/RecipePage";
 function App() {
   return (
     <AuthProvider>
-      <BrowserRouter basename="/noixlecafe-cheatsheet">
-        <Routes>
-          <Route path="/" element={<Navigate to="/login" replace />} />
-          <Route path="/login" element={<Login />} />
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
 
-          <Route element={<ProtectedRoute />}>
-            <Route path="/main" element={<ColdOrHot />} />
-            <Route path="/hot" element={<RecipePage type='hot'/>} />
-            <Route path="/cold" element={<RecipePage type='cold'/>} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+        <Route element={<ProtectedRoute />}>
+          <Route path="/main" element={<ColdOrHot />} />
+          <Route path="/hot" element={<RecipePage type='hot' />} />
+          <Route path="/cold" element={<RecipePage type='cold' />} />
+        </Route>
+      </Routes>
     </AuthProvider>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './components/App'
 import { AuthProvider } from './contexts/AuthContext'
 import "./index.css"
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthProvider>
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     </AuthProvider>
   </StrictMode>,
 )
